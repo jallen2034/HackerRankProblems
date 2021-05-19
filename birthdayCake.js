5abafc5fbd966a67732e552b6db8bd8661b97fee
jallen2034 <jallen2033@gmail.com>
2021-05-19 16:33:43 -0700

    Added working birthday cake soloution

---
+++ birthdayCake.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function (inputStdin) {
+  inputString += inputStdin;
+});
+
+process.stdin.on('end', function () {
+  inputString = inputString.split('\n');
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'birthdayCakeCandles' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY candles as parameter.
+ */
+
+function birthdayCakeCandles(candles) {
+  // Write your code here
+  let highest = 0;
+  let highestCount = 0;
+  const arrLen = candles.length; // collect this once for optimization
+
+  // loop through array of candles initally and calculate the highest count seen 
+  for (let i = 0; i < arrLen; i++) {
+    if (candles[i] > highest) {
+      highest = candles[i];
+    }
+  }
+
+  // loop through array of candles initally and calculate the highest count seen 
+  for (let i = 0; i < arrLen; i++) {
+    if (candles[i] === highest) {
+      highestCount += 1;
+    }
+  }
+
+  return highestCount;
+}
+
+function main() {
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+  const candlesCount = parseInt(readLine().trim(), 10);
+
+  const candles = readLine().replace(/\s+$/g, '').split(' ').map(candlesTemp => parseInt(candlesTemp, 10));
+
+  const result = birthdayCakeCandles(candles);
+
+  ws.write(result + '\n');
+
+  ws.end();
+}
